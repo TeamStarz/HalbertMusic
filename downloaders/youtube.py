c4209ec0cad956a55332e4ffe54e10b409880d34
TeamStarz <83304626+TeamStarz@users.noreply.github.com>
2021-06-14 22:47:57 +0700

    Update youtube.py

--- downloaders/youtube.py
+++ downloaders/youtube.py
@@ -20,12 +20,12 @@ def download(url: str) -> str:
 
     if duration > DURATION_LIMIT:
         raise DurationLimitError(
-            f"❌ Durasi video lebih lama dari {DURATION_LIMIT} minute(s) tidak diperbolehkan, durasi video yang disediakan adalah {duration} minute(s)"
+            f"❌ Durasi Video Lebih Lama Dari {DURATION_LIMIT} Minute(s) Tidak Di Perbolehkan, Durasi video Yang Di Sediakan Adalah {duration} Minute(s)"
         )
     try:
         ydl.download([url])
     except:
         raise DurationLimitError(
-            f"❌ Durasi video lebih lama dari {DURATION_LIMIT} minute(s) tidak diperbolehkan, durasi video yang disediakan adalah {duration} minute(s)"
+            f"❌ Durasi Video Lebih Lama Dari {DURATION_LIMIT} Minute(s) Tidak Di Perbolehkan, Durasi Video Yang Di Sediakan Adalah {duration} Minute(s)"
         )
     return path.join("downloads", f"{info['id']}.{info['ext']}")
