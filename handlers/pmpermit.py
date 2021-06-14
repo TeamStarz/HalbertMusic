71e0c53fca2291df63ecd2b72ed820e6a31dcb43
TeamStarz <83304626+TeamStarz@users.noreply.github.com>
2021-06-14 21:41:19 +0700

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Alooo Ges 🤩 Saya adalah **Layanan Asisten Irama Musik Bot.**\n\n ❗️ Rules :\n   - Biasakan salam, amanat dari [Owner](t.me/IamYourEnemy) \n   - Jangan spam pemutaran lagu agar bot tidak error \n\n **Note :** Kirim link undangan grup atau username grup jika userbot/asisten tidak dapat bergabung dengan grup anda atau yang lainnya.\n\n 🛠️ Ke [Group Support](t.me/VcgSupportGroup) atau bisa PC ke owner @IamYourEnemy ⚡\n\n")
+  await USER.send_message(message.chat.id,"Hallo Saya Adalah **Layanan Asisten Zeed - Music Bot.**\n\n ❗️ Rules :\n   - Jangan Melakukan Spam Pesan.\n   - Jangan Spam Pemutaran Lagu.\n   - Jangan Lupa Join Group Music : @ZeeedMusic.\n\n **Note :** Kirim Link Undangan Grup Atau Username Grup Jika Userbot/Asisten Tidak Dapat Bergabung Dengan Grup Anda Atau Yang Lainnya.\n\n Dikelola Oleh : @Reeeeeezy.\n\n")
   return                        
