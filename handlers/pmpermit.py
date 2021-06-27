0bfc3a13d078ac964a08d703ed047b80763e7afb
TeamStarz <83304626+TeamStarz@users.noreply.github.com>
2021-06-27 14:01:16 +0700

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Hallo Saya Adalah **Layanan Asisten Zeed - Music Bot.**\n\n ❗️ Rules :\n   - Jangan Melakukan Spam Pesan.\n   - Jangan Spam Pemutaran Lagu.\n   - Jangan Lupa Join Group Music : @ZeeedMusic.\n\n **Note :** Kirim Link Undangan Grup Atau Username Grup Jika Userbot/Asisten Tidak Dapat Bergabung Dengan Grup Anda Atau Yang Lainnya.\n\n Dikelola Oleh : @Reeeeeezy.\n\n")
+  await USER.send_message(message.chat.id,"Hallo Saya Adalah **Layanan Asisten Libra Robot.**\n\n ❗️ Rules :\n   - Jangan Melakukan Spam Pesan.\n   - Jangan Spam Pemutaran Lagu.\n\n **Note :** Kirim Link Undangan Grup Atau Username Grup Jika Userbot/Asisten Tidak Dapat Bergabung Dengan Grup Anda Atau Yang Lainnya.\n   ◉ **Channel :** @Rezy_IsBack.\n   ◉ **Dikelola Oleh :** @Reeeeeezy.\n   ◉ **Instagram :** [@ridhoalfahrezi._](https://www.instagram.com/ridhoalfahrezi._)\n\n")
   return                        
