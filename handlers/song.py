80de692a821d3fc6a9f92b252c89591b65742091
TeamStarz <83304626+TeamStarz@users.noreply.github.com>
2021-06-27 13:55:12 +0700

    Update song.py

--- handlers/song.py
+++ handlers/song.py
@@ -55,13 +55,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("Sedang Mendownload Lagu...")
+    m.edit("⏳ Sedang Mendownload Lagu...")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**🎶 Uploaded by [Rezy](https://t.me/Reeeeeezy).**'
+        rep = '**🎶 Uploaded by [Rezy.](https://t.me/Reeeeeezy)**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
